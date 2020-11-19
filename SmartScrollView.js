ee7afcdf786b2f20924548d4263c7dc7a76bb189
David Wheatley <hi@davwheat.dev>
2020-11-19 22:00:27 +0000

    Don't import PropTypes from React!

--- SmartScrollView.js
+++ SmartScrollView.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import ReactNative, { View, ScrollView, Keyboard, Dimensions, LayoutAnimation, Platform } from 'react-native';
 
